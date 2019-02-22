f90d60759003782411541da9c75e53b14dd0abe1
tuantx <tuantx@qsoftvietnam.com>
2019-02-22 10:51:08 +0700

    share detail

--- app/src/main/java/com/dz/ui/fragments/history/detail/DetailFragment.kt
+++ app/src/main/java/com/dz/ui/fragments/history/detail/DetailFragment.kt
@@ -6,7 +6,6 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.ActionBar
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
 import com.dz.models.responses.BookResponse
@@ -18,7 +17,9 @@ import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import android.util.Log
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dz.libraries.utilities.StringUtility
+import com.dz.utilities.AppUtility
 import com.google.android.youtube.player.YouTubePlayerSupportFragment
 
 
@@ -33,7 +34,7 @@ class DetailFragment : BaseMainFragment<IDetailView, IDetailPresenter>(), IDetai
     override val resourceId: Int get() = R.layout.history_detail_fragment
 
     override val titleId: Int get() = R.string.detail
-    private var VIDEO_ID = ""
+    private var VIDEO_ID = "ZLNO2c7nqjw"
 
     var favorite: Boolean = true
 
@@ -42,19 +43,17 @@ class DetailFragment : BaseMainFragment<IDetailView, IDetailPresenter>(), IDetai
         bindButterKnife(view)
         setHasOptionsMenu(true)
         initView()
-        initYoutubePlayer()
+        initYoutubePlayer(VIDEO_ID)
     }
 
-    fun initYoutubePlayer() {
+    fun initYoutubePlayer(videoId: String) {
         val youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance()
         transaction = childFragmentManager.beginTransaction()
         transaction.add(R.id.youtube_layout, youTubePlayerFragment).commit()
         youTubePlayerFragment.initialize(Constant.API_KEY, object : YouTubePlayer.OnInitializedListener {
             override fun onInitializationSuccess(p0: YouTubePlayer.Provider, player: YouTubePlayer, p2: Boolean) {
-                //TODO video id youtube
-                VIDEO_ID = "ZLNO2c7nqjw"
                 player.setFullscreen(false)
-                player.loadVideo(VIDEO_ID)
+                player.loadVideo(videoId)
                 player.play()
             }
 
@@ -72,11 +71,10 @@ class DetailFragment : BaseMainFragment<IDetailView, IDetailPresenter>(), IDetai
             val link = it!!.link!!
             if (!StringUtility.isNullOrEmpty(link)) {
                 if (link.contains("v="))
-                    VIDEO_ID = link.split("v=")[1]
-                initYoutubePlayer()
+                    initYoutubePlayer(link.split("v=")[1])
             }
         }
-        rvLinks.layoutManager = GridLayoutManager(mActivity, 2)
+        rvLinks.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         rvLinks.isNestedScrollingEnabled = false
         rvLinks.adapter = detailAdapter
     }
@@ -114,7 +112,7 @@ class DetailFragment : BaseMainFragment<IDetailView, IDetailPresenter>(), IDetai
 
             }
             R.id.action_share -> {
-
+                AppUtility.shareSocial(mActivity)
             }
         }
         return super.onOptionsItemSelected(item)
--- app/src/main/java/com/dz/ui/fragments/history/detail/adapter/DetailAdapter.kt
+++ app/src/main/java/com/dz/ui/fragments/history/detail/adapter/DetailAdapter.kt
@@ -2,14 +2,12 @@ package com.dz.ui.fragments.history.detail.adapter
 
 import android.content.Context
 import android.view.View
-import androidx.appcompat.widget.AppCompatImageView
 import butterknife.BindView
 import com.dz.libraries.views.recyclerviews.ExtRecyclerViewAdapter
 import com.dz.libraries.views.recyclerviews.ExtRecyclerViewHolder
 import com.dz.libraries.views.textviews.ExtTextView
 import com.dz.models.responses.BookResponse
 import com.dz.ui.R
-import com.dz.utilities.PicasoUtility
 
 class DetailAdapter(ctx: Context, its: ArrayList<BookResponse?>?, var itemConsumer: (BookResponse?) -> Unit) : ExtRecyclerViewAdapter<BookResponse, DetailAdapter.ViewHolder>(ctx, its) {
     override fun getLayoutId(viewType: Int): Int = R.layout.item_history_detail
@@ -18,18 +16,15 @@ class DetailAdapter(ctx: Context, its: ArrayList<BookResponse?>?, var itemConsum
 
     override fun onBindViewHolder(holder: ViewHolder, data: BookResponse?, position: Int) {
         holder.tvTitle.text = data?.name
-        PicasoUtility.get()
-                .load(data!!.link)
-                .placeholder(R.drawable.bg_background_radius_component)
-                .into(holder.ivPlayVideo)
+        holder.numberHistory.text = (position + 1).toString()
         holder.itemView.setOnClickListener { itemConsumer(data) }
     }
 
     class ViewHolder(itemView: View) : ExtRecyclerViewHolder(itemView) {
         @BindView(R.id.tvTitle)
         lateinit var tvTitle: ExtTextView
-        @BindView(R.id.ivPlayVideo)
-        lateinit var ivPlayVideo: AppCompatImageView
+        @BindView(R.id.numberHistory)
+        lateinit var numberHistory: ExtTextView
 
     }
 }
